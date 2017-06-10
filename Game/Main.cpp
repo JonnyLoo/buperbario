ba2d397a09576c32e74dabec70d9fe24f9563ac8
JonnyLoo <jonliu44@gmail.com>
2017-06-09 21:27:07 -0400

    fixed main location

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main() {
 	cout << "Itsa b BARIO!!!";
+	int gameState = 1;
+	while(gameState) {
+
+		gameLoop();
+	}
 	return 0;
+}
+
+void gameLoop() {
+
+
+}
+
+void update() {
+
+
+}
+
+void render() {
+
+	
 }

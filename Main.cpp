59a60c66c9f8b0c450d3c5a8235b22eb1e987a3c
JonnyLoo <jonliu44@gmail.com>
2017-06-09 21:26:06 -0400

    setting up main

---
+++ Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Itsa b BARIO!!!";
+	int gameState = 1;
+	while(gameState) {
+
+		gameLoop();
+	}
+	return 0;
+}
+
+void gameLoop() {
+
+
+}
+
+void update() {
+
+
+}
+
+void render() {
+
+	
+}
